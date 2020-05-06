82b913e4fcec2cc9d525f7bf0fce374b2728bd39
Balthazar de Moncuit <balthazar.de.moncuit@gmail.com>
2020-05-06 17:14:52 +0200

    Add resolvers and services needed for SubscriptionToEvenement

--- src/subscription-to-evenement/subscription-to-evenement.resolver.ts
+++ src/subscription-to-evenement/subscription-to-evenement.resolver.ts
@@ -1,13 +1,100 @@
 import { SubscriptionToEvenement } from "./subscription-to-evenement.model";
-import { UseGuards } from "@nestjs/common";
+import { UseGuards, UnauthorizedException, ForbiddenException } from "@nestjs/common";
 import { GqlAuthGuard } from "src/auth/gql-auth.guard";
-import { ResolveField, Parent, Resolver } from "@nestjs/graphql";
+import { ResolveField, Parent, Resolver, Mutation, Args } from "@nestjs/graphql";
 import { SubscriptionToEvenementService } from "./subscription-to-evenement.service";
+import { UserEntity } from "src/user/user.decorator";
+import { User, Role } from "src/user/user.model";
+import { Connection } from "typeorm";
+import { Evenement } from "src/evenement/evenement.model";
 
 @Resolver((of) => SubscriptionToEvenement)
 @UseGuards(GqlAuthGuard)
 export class SubscriptionToEvenementResolver {
-  constructor(private subscriptionToEvenementService: SubscriptionToEvenementService) {}
+  constructor(
+    private subscriptionToEvenementService: SubscriptionToEvenementService,
+    private connection: Connection,
+  ) {}
+
+  @Mutation((returns) => SubscriptionToEvenement)
+  async createSubscriptionToEvenement(
+    @Args('userId') userId: string,
+    @Args('evenementId') evenementId: string,
+    @UserEntity() user: User,
+  ) {
+    if (user.role !== Role.ADMIN) {
+      if (userId !== user.id) {
+        throw new UnauthorizedException(`Une inscription ne peut être fait que par l\'utilisateur s\'inscrivant lui même où un ${Role.ADMIN}`);
+      }
+    }
+
+    const evenement = await this.connection.createQueryBuilder(Evenement, 'evenement').whereInIds([evenementId]).getOne();
+    if (!evenement.isActive) {
+      throw new ForbiddenException('L\'inscription à un évenement non actif est prohibé.');
+    }
+
+    return this.subscriptionToEvenementService.createSubscriptionToEvenement({
+      userId: user.id,
+      evenementId,
+      isInstructor: false,
+      isValid: false,
+      isCertified: false,
+    });
+  }
+
+  @Mutation((returns) => SubscriptionToEvenement)
+  async addFormatorToEvenement(
+    @Args('userId') userId: string,
+    @Args('evenementId') evenementId: string,
+    @UserEntity() user: User,
+  ) {
+    if (user.role !== Role.ADMIN) {
+      throw new UnauthorizedException(`Seul un ${Role.ADMIN} peut désigner un ${Role.FORMATOR} sur un évenement`);
+    }
+
+    const formator = await this.connection.createQueryBuilder(User, 'user').whereInIds([userId]).getOne();
+    if (!formator || formator.role === Role.PARTICIPANT) {
+      throw new ForbiddenException(`L\'utilisateur ne peut être désigné ${Role.FORMATOR}. Vérifiez qu\'il existe et que son role est ${Role.FORMATOR} ou ${Role.ADMIN}`);
+    }
+
+    return this.subscriptionToEvenementService.createSubscriptionToEvenement({
+      userId: user.id,
+      evenementId,
+      isInstructor: true,
+      isValid: true,
+      isCertified: false,
+    });
+  }
+
+  @Mutation((returns) => SubscriptionToEvenement, { description: `Permet de valider l'inscription (à été payé/visé)` })
+  async validateSubscriptionToEvenement(
+    @Args('subscriptionToEvenementId') subscriptionToEvenementId: string,
+    @UserEntity() user: User,
+  ) {
+    if (user.role !== Role.ADMIN) {
+      throw new UnauthorizedException(`Seul un ${Role.ADMIN} peut valider une inscription`);
+    }
+
+    return this.subscriptionToEvenementService.updateSubscriptionToEvenement({
+      id: subscriptionToEvenementId,
+      isValid: true,
+    });
+  }
+
+  @Mutation((returns) => SubscriptionToEvenement, { description: `Permet de certifier l'inscription (et donc l'utilisateur lié)` })
+  async certifySubscriptionToEvenement(
+    @Args('subscriptionToEvenementId') subscriptionToEvenementId: string,
+    @UserEntity() user: User,
+  ) {
+    if (user.role !== Role.ADMIN) {
+      throw new UnauthorizedException(`Seul un ${Role.ADMIN} peut certifier une inscription`);
+    }
+
+    return this.subscriptionToEvenementService.updateSubscriptionToEvenement({
+      id: subscriptionToEvenementId,
+      isCertified: true,
+    });
+  }
 
   @ResolveField('user')
   async user(@Parent() subscriptionToEvenement: SubscriptionToEvenement) {
--- src/subscription-to-evenement/subscription-to-evenement.service.ts
+++ src/subscription-to-evenement/subscription-to-evenement.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { SubscriptionToEvenement } from './subscription-to-evenement.model';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -24,5 +24,33 @@ export class SubscriptionToEvenementService {
       where: subscriptionToEvenement,
       relations: ['evenement'],
     })).evenement;
+  }
+
+  async createSubscriptionToEvenement(data: {
+    userId: string,
+    evenementId: string,
+    isInstructor: boolean,
+    isValid: boolean,
+    isCertified: boolean,
+  }) {
+    return this.subscriptionToEvenementRepository.save(data);
+  }
+
+  async updateSubscriptionToEvenement(data: {
+    id: string,
+    isValid?: boolean,
+    isCertified?: boolean,
+  }) {
+    let subscriptionToEvenement = await this.subscriptionToEvenementRepository.findOne(data.id);
+    if (!subscriptionToEvenement) {
+      throw new NotFoundException(`L'inscription en question n'existe pas en base de donnée`);
+    }
+
+    if (data.isValid !== undefined) subscriptionToEvenement.isValid = data.isValid;
+    if (data.isCertified !== undefined) subscriptionToEvenement.isCertified = data.isCertified;
+
+    await this.subscriptionToEvenementRepository.update(data.id, subscriptionToEvenement);
+
+    return subscriptionToEvenement;
   }
 }
